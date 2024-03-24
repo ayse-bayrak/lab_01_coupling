9555983fa93e504ba1e740fe08d612577d4c0a9f
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-24 11:41:29 +0300

    little changes

--- src/main/java/com/cydeo/loosely/BalanceService.java
+++ src/main/java/com/cydeo/loosely/BalanceService.java
@@ -1,4 +1,41 @@
 package com.cydeo.loosely;
 
+import com.cydeo.tightly.CustomerBalance;
+import com.cydeo.tightly.GiftCardBalance;
+
+import java.math.BigDecimal;
+
 public class BalanceService {
+
+    Balance customerBalance;
+    Balance giftCardBalance;
+
+    public BalanceService(Balance customerBalance, Balance giftCardBalance) {
+        this.customerBalance = customerBalance;
+        this.giftCardBalance = giftCardBalance;
+    }
+
+    public boolean checkoutFromCustomerBalance(BigDecimal checkoutAmount) {
+
+        BigDecimal customerBalanceAmount = this.customerBalance.getAmount();
+
+        if (customerBalanceAmount.subtract(checkoutAmount).compareTo(BigDecimal.ZERO) >= 0) {
+            this.customerBalance.setAmount(customerBalanceAmount.subtract(checkoutAmount));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkoutFromGiftCardBalance(BigDecimal checkoutAmount) {
+
+        BigDecimal giftCardBalanceAmount = this.giftCardBalance.getAmount();
+
+        if (giftCardBalanceAmount.subtract(checkoutAmount).compareTo(BigDecimal.ZERO) >= 0) {
+            this.giftCardBalance.setAmount(giftCardBalanceAmount.subtract(checkoutAmount));
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/cydeo/loosely/CouplingTestRunner.java
+++ src/main/java/com/cydeo/loosely/CouplingTestRunner.java
@@ -1,4 +1,29 @@
 package com.cydeo.loosely;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class CouplingTestRunner {
+    public static void main(String[] args) {
+
+        UUID user = UUID.randomUUID();
+
+        Balance customerBalance = new CustomerBalance(user, BigDecimal.ZERO);
+        Balance giftCardBalance = new GiftCardBalance(user, BigDecimal.ZERO);
+
+        BalanceService balanceService = new BalanceService(customerBalance, giftCardBalance);
+
+        customerBalance.addBalance(BigDecimal.valueOf(100));
+        giftCardBalance.addBalance(BigDecimal.valueOf(100));
+
+        System.out.println("CUSTOMER BALANCE:");
+        System.out.println(customerBalance.getAmount()); // 100
+        System.out.println(balanceService.checkoutFromCustomerBalance(BigDecimal.valueOf(50)));//true
+        System.out.println(customerBalance.getAmount()); // 50
+
+        System.out.println("GIFT CARD BALANCE:");
+        System.out.println(giftCardBalance.getAmount()); // 110
+        System.out.println(balanceService.checkoutFromGiftCardBalance(BigDecimal.valueOf(50)));
+        System.out.println(giftCardBalance.getAmount()); // 60
+    }
 }
